a86b0f74913c6174d10b9c84bec3a0a54ca19f42
Alexandre Amrani <alexandre@amrani.fr>
2017-10-21 16:41:44 +0200

    3 tabs

---
+++ app/src/main/java/com/example/alexandre/tabbedairforce/Tab1.java
@@ -0,0 +1,22 @@
+package com.example.alexandre.tabbedairforce;
+
+/**
+ * Created by alexandre on 21/10/17.
+ */
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1, container, false);
+        return rootView;
+    }
+
+}
---
+++ app/src/main/java/com/example/alexandre/tabbedairforce/Tab2.java
@@ -0,0 +1,22 @@
+package com.example.alexandre.tabbedairforce;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by alexandre on 21/10/17.
+ */
+
+public class Tab2  extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2, container, false);
+        return rootView;
+    }
+
+}
